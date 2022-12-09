2e4d80cb5c3b885a02d5a04c618f593ce4f523a7
Tahir <dibirovtahir@gmail.com>
2022-12-09 23:01:26 +0400

    INIT

---
+++ main.js
@@ -0,0 +1,36 @@
+const carousel = document.querySelector('.carousel')
+const URL = 'https://random.imagecdn.app/v1/image?width=500&height=150&category=buildings&format=json'
+const imageContent = document.querySelector('.image-content')
+let imageSet = []
+
+
+const setImage = async (URL) => {
+
+    let i = 0
+    setInterval(async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        imageContent.innerHTML = `<img src="" alt="" class="image">`
+        imageSet[i] = data.url
+        i++
+        if (i === 5) i = 0
+        console.log(imageSet)
+        carousel.querySelector('img').src = imageSet[0]
+
+
+    }, 2000)
+
+
+
+}
+
+carousel.querySelector('.next').addEventListener('click', ()=>{
+    carousel.querySelector('img').src = imageSet[imageSet.indexOf(carousel.querySelector('img').src)+1]
+})
+
+carousel.querySelector('.previous').addEventListener('click', ()=>{
+    carousel.querySelector('img').src = imageSet[imageSet.indexOf(carousel.querySelector('img').src)-1]
+})
+
+setImage(URL)
+
